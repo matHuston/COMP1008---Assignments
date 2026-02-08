6f638de1b85a0bd626945a030ab6e233117cc897
matHuston <thecaranar@gmail.com>
2026-02-08 17:46:35 -0500

    constructor added

--- lab_3/Employee.java
+++ lab_3/Employee.java
@@ -6,10 +6,25 @@ public abstract class Employee {
     private String socialSecurityNumber;
  
     // TODO 1: Create a constructor
+    public abstract class Employee {
+        private String firstName;
+        private String lastName;
+        private String socialSecurityNumber;
+    
+        //constructor to initialize the three instance variables
+        public Employee(String first, String last, String ssn){
+            this.firstName = first;
+            this.lastName = last;
+            this.socialSecurityNumber = ssn;
+        }
+    }
  
     // TODO 2: Create getters
+    
  
     // TODO 3: Declare abstract method earnings()
+    
  
     // TODO 4: Override toString()
+    
 }
