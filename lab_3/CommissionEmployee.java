93d6b7ac211db93830faf433c8d85a1faba466f5
matHuston <thecaranar@gmail.com>
2026-02-08 17:51:48 -0500

    setters and getters with validation added

--- lab_3/CommissionEmployee.java
+++ lab_3/CommissionEmployee.java
@@ -11,10 +11,24 @@ public class CommissionEmployee extends Employee {
         setGrossSales(sales);
         setCommissionRate(rate);
     }
-
-    // TODO 6: Create getters and setters with validation
-    //setters with validation
     
+    // TODO 6: Create getters and setters with validation
+    // setters with validation
+    public void setGrossSales(double sales){
+        if(sales < 0.0){
+            throw new IllegalArgumentException("Gross sales must be >= 0.0");
+        }
+        grossSales = sales;
+    }
+    public void setCommissionRate(double rate){
+        if(rate <= 0.0 || rate >= 1.0){
+            throw new IllegalArgumentException("Commission rate must be > 0.0 and < 1.0");
+        }
+        commissionRate = rate;
+    }
+    // getters
+    public double getGrossSales(){return grossSales;}
+    public double getCommissionRate(){return commissionRate;}
  
     // TODO 7: Override earnings()
  
