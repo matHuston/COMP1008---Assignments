e46a4663614fa7cc5e8096ea164580ceae93b4a1
matHuston <thecaranar@gmail.com>
2026-02-08 17:57:32 -0500

    CommissionEmployee object added, displaying employee details, and displaying earnings

--- lab_3/PayrollTest.java
+++ lab_3/PayrollTest.java
@@ -2,11 +2,16 @@ package lab_3;
 public class PayrollTest {
  
     public static void main(String[] args) {
- 
+
         // TODO 9: Create a CommissionEmployee object
- 
+        CommissionEmployee cEmployee = new CommissionEmployee("Mat", "Huston", "705-420-1337", 6000, .05);
+        
         // TODO 10: Display employee details
+        System.out.println(cEmployee); // invokes toString()
  
         // TODO 11: Display earnings
+        System.out.printf("Earnings: $%.2f%n", cEmployee.earnings());
     }
+
+    
 }
