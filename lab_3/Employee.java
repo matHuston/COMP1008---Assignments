c95e2378b7e6266d99bc273dfed2610b39a8d336
matHuston <thecaranar@gmail.com>
2026-02-08 18:00:57 -0500

    fix: i made an extra constructor by accident oops

--- lab_3/Employee.java
+++ lab_3/Employee.java
@@ -6,10 +6,7 @@ public abstract class Employee {
     private String socialSecurityNumber;
  
     // TODO 1: Create a constructor
-    public abstract class Employee {
-        private String firstName;
-        private String lastName;
-        private String socialSecurityNumber;
+    
     
         //constructor to initialize the three instance variables
         public Employee(String first, String last, String ssn){
@@ -17,7 +14,7 @@ public abstract class Employee {
             this.lastName = last;
             this.socialSecurityNumber = ssn;
         }
-    }
+
  
     // TODO 2: Create getters
     public String getFirstName() {
